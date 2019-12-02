cb82c698b1669180ca5e68066b3ab4097ba92559
James Crawford <58075831+yooxiang@users.noreply.github.com>
2019-12-02 11:34:44 +0000

    updated
    made the funtions more useable for later development

--- master.py
+++ master.py
@@ -1,16 +1,17 @@
 import matplotlib.pyplot as plt
 import scipy as sp
+import math
 data = sp.loadtxt("DATA_ONE.txt",encoding='utf-8-sig')
 data_2 = sp.loadtxt("DATA_TWO.txt",encoding='utf-8-sig')
 
 R = sum(data)/sum(data_2)
 
-#function
+#function oscilation
 def p(E,theta, m, L):
     return 1 - (sp.sin(2*theta)**2)*sp.sin((1.267*(m**2)*L)/E)**2
 
 #plots
-E = sp.linspace(0,10,len(data))
+E = sp.linspace(0.05,10.0,len(data))
 plt.figure()
 plt.xlabel("energy (GeV)")
 plt.ylabel("count")
@@ -31,20 +32,27 @@ plt.ylabel("probablity")
 plt.title("model")
 
 plt.figure()
-def lamda(E,theta): #returns the prediction as a function of E
-    return R*(E*p(0.05*i,theta,sp.sqrt(2.4e-3),295))
+
+def Lam(Energy,u):
+    index = math.floor(Energy/0.05) - 1
+    return data_2[index]*p(Energy,u,sp.sqrt(2.4e-3),295)
     
 prediction = []
 for i in range(len(data_2)):
-    prediction.append(lamda(data_2[i],sp.pi/4))
-plt.bar(E,prediction,width= 0.05)
+    prediction.append(Lam(E[i],sp.pi/4))
 
+plt.title("simulation")
+plt.bar(E,prediction,width = 0.05)
+
+#%%
 def NLL(u,data):
     x = 0
     for i in range(len(data)):
-        x += lamda(i*0.05,u) - data[i] - data[i]*sp.log(data[i]/lamda(i*0.05,u))
+        x += Lam((0.05)+i*0.05,u) - data[i] - data[i]*sp.log(data[i]/Lam((0.05)+i*0.05,u))
+    return x
     
 plt.figure()    
-theta = sp.linspace(0,sp.pi,100)
-plt.bar(theta,NLL(theta,data))
+theta = sp.linspace(0,sp.pi/4,500)
+
+plt.plot(theta,NLL(theta,data))
         
